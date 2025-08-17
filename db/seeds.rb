ddcedc609d410229d2a99a3a9b464e375e5d7036
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-17 16:04:09 +0800

    test: Add Flight seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,38 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-[ "DXB", "HND", "LAX", "MNL", "MAD", "SIN", "MIA", "HKG", "ICN" ].each do |code|
-  Airport.find_or_create_by!(code: code)
+require 'faker'
+
+$airports = %w[DXB HDN LAX MNL MAD SIN MIA HKG ICN]
+
+def seed_airports
+  $airports.each do |code|
+    Airport.find_or_create_by!(code: code)
+  end
 end
+
+def seed_flights
+  10.times do
+    Flight.find_or_create_by!(random_flight_data)
+  end
+end
+
+def random_flight_data
+  date = Faker::Time.between(from: DateTime.now, to: 1.year.from_now, format: :default)
+  duration = Faker::Number.between(from: 120, to: 1100)
+  departure = random_airport
+  arrival = random_airport
+
+  until departure != arrival
+    arrival = random_airport
+  end
+
+  { date: date, duration: duration, departure_airport: departure, arrival_airport: arrival }
+end
+
+def random_airport
+  Airport.find_by(code: $airports.sample)
+end
+
+seed_airports
+seed_flights
