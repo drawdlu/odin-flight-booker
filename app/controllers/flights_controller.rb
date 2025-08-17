596bf11fe97fb2588dd35246da096b430e11c041
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-17 19:12:55 +0800

    feat: Add a search form for flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,11 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    @flight = Flight.new
+    @departures = Flight.all.map { |f| [ f.departure_airport.code, f.departure_airport.id ] }.uniq
+    @arrivals = Flight.all.map { |f| [ f.arrival_airport.code, f.arrival_airport.id ] }.uniq
+    @dates = Flight.all.map { |f| [ f.date.strftime("%m/%d/%Y") ] }
+  end
+
+  def show
   end
 end
