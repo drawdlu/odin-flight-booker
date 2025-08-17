ac958fb44a0d10b344e28dc4c2a00973762db4c2
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-17 19:57:09 +0800

    fix: Date data blank in parameters
    Forgot to add value for date

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,7 @@
 class FlightsController < ApplicationController
   def index
-    @flight = Flight.new
     @departures = Flight.all.map { |f| [ f.departure_airport.code, f.departure_airport.id ] }.uniq
     @arrivals = Flight.all.map { |f| [ f.arrival_airport.code, f.arrival_airport.id ] }.uniq
-    @dates = Flight.all.map { |f| [ f.date.strftime("%m/%d/%Y") ] }
-  end
-
-  def show
+    @dates = Flight.all.map { |f| [ f.date.strftime("%m-%d-%Y"), f.date.strftime("%Y-%m-%d") ] }
   end
 end
