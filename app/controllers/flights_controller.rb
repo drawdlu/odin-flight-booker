1b1afec5ac4de39d2be150f2d284631c877864d5
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-18 19:06:00 +0800

    feat: Add select form for choosing a flight
    Also include a hidden input for passenger_count that was chosen on
    search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,10 +7,19 @@ class FlightsController < ApplicationController
     unless params["search"].nil?
       @flights = Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND DATE(date) = ?",
                 search_params["departure_airport_id"], search_params["arrival_airport_id"], search_params["date"])
+      @flights = @flights.map { |f| [ to_string(f), f.id ] }
+      @passenger_count = search_params["passenger_count"]
     end
   end
 
   private
+
+  def to_string(flight)
+    "Departure Airport: #{flight.departure_airport.code}
+    Arrival Airport: #{flight.arrival_airport.code}
+    Date: #{flight.date.strftime("%m-%d-%Y")}
+    Time: #{flight.date.strftime("%H-%M")}"
+  end
 
   def search_params
     params.require(:search).permit(:departure_airport_id, :arrival_airport_id, :passenger_count, :date)
