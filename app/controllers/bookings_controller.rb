05fab1106573e181a873a163e512ccde8cd9bb38
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-18 19:41:07 +0800

    feat: Display flight information on booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,11 @@
 class BookingsController < ApplicationController
+  def new
+    @flight = Flight.find(booking_params["flight_id"])
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, :passenger_count)
+  end
 end
