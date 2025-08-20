d71c731fdde5ac9ecc82dd2023f5767259eee520
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-20 16:47:00 +0800

    fix: Schema booking references passengers
    Passenger model should reference Booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_18_112447) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_20_084547) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,11 +22,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_18_112447) do
 
   create_table "bookings", force: :cascade do |t|
     t.bigint "flight_id", null: false
-    t.bigint "passenger_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["flight_id"], name: "index_bookings_on_flight_id"
-    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -45,10 +43,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_18_112447) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "booking_id", null: false
+    t.index ["booking_id"], name: "index_passengers_on_booking_id"
   end
 
   add_foreign_key "bookings", "flights"
-  add_foreign_key "bookings", "passengers"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
+  add_foreign_key "passengers", "bookings"
 end
